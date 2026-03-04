ad31175af2c4e2e99ff42dd640e5c76a7817eb68
shihab-63 <thecodzhub@gmail.com>
2026-03-04 19:39:01 +0600

    made task section responsive

--- src/components/TicketDashboard/TicketDashboard.jsx
+++ src/components/TicketDashboard/TicketDashboard.jsx
@@ -9,10 +9,10 @@ const TicketDashboard = ({ ticketsPromise }) => {
   return (
     <div>
       <Container>
-        <div className="grid md:grid-cols-12 gap-7">
+        <div className="grid grid-cols-1 md:grid-cols-12 gap-7">
           {/* Left Side Ticket */}
-          <div className="order-2 md:order-1 col-span-9">
-            <h1 className="text-2xl font-bold">Customer Tickets</h1>
+          <div className="order-2 md:order-1 md:col-span-9">
+            <h1 className="mx-3 md:mx-0 text-2xl font-bold">Customer Tickets</h1>
             <div className="grid grid-cols-1 mb-10 mx-3 md:mx-0 md:grid-cols-2 gap-6 mt-5">
               {ticketsData.map((ticket, index) => (
                 <Ticket key={index} ticket={ticket} />
@@ -20,7 +20,7 @@ const TicketDashboard = ({ ticketsPromise }) => {
             </div>
           </div>
           {/* Right Side Task */}
-          <div className="order-1 md:order-1 col-span-3 h-fit">
+          <div className="order-1 mx-3 md:mx-0 md:order-1 col-span-1 md:col-span-3 h-fit">
             <TaskStatus />
             <ResolvedTask />
           </div>
